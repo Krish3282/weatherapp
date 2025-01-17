1793aa31e29b25c07ffcb8e609e68c624b80f1ab
Krish3282 <kvkrishna1938@gmail.com>
2025-01-17 09:46:23 +0530

    Updated Weather.jsx to fix issues

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -2,16 +2,33 @@ import React, { useEffect, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
-// import cloud_icon from '../assets/cloudsearch.png'
-// import drizzle_icon from '../assets/drizzle.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
 import humidity_icon from '../assets/humidity.png'
-// import rain_icon from '../assets/rain.png'
-// import snow_icon from '../assets/snow.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 
 
 function Weather() {
   const [weatherData,setWeatherData] =  useState(false);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d":cloud_icon ,
+    "02n": cloud_icon,
+    "03d":cloud_icon,
+    "03n":cloud_icon,
+    "04d":drizzle_icon,
+    "04n":drizzle_icon,
+    "09d":rain_icon,
+    "09n":rain_icon,
+    "10d":rain_icon,
+    "10n":rain_icon,
+    "13d":snow_icon,
+    "13n":snow_icon,
+  }
 
   const search = async (city)=>{
     try {
@@ -19,12 +36,14 @@ function Weather() {
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+
       setWeatherData({
         humidity:data.main.humidity,
         windSpeed : data.wind.speed,
         temperature : Math.floor(data.main.temp),
         loaction : data.name,
-        icon : h
+        icon : icon,
       })
 
     } catch (error) {
@@ -32,7 +51,7 @@ function Weather() {
     }
   }
   useEffect(()=>{
-    search("Chennai");
+    search("London");
   },[])
   return (
     <div className='weather'>
@@ -41,21 +60,21 @@ function Weather() {
         <img src={search_icon} alt="" />
       </div>
 
-      <img src={clear_icon} className='weather-icon' alt="" />
-      <p className='temperature'>16°C</p>
-      <p className='location'>London</p>
+      <img src={weatherData.icon} className='weather-icon' alt="" />
+      <p className='temperature'>{weatherData.temperature}°C</p>
+      <p className='location'>{weatherData.loaction}</p>
       <div className="weather-data">
         <div className="col">
           <img src={humidity_icon} alt="" />
           <div>
-            <p>91%</p>
+            <p>{weatherData.humidity}%</p>
             <span>Humidity</span>
           </div>
         </div>
         <div className="col">
           <img src={wind_icon} alt="" />
           <div>
-            <p>3.6 Km/h</p>
+            <p>{weatherData.windSpeed} Km/h</p>
             <span>Wind Speed</span>
           </div>
         </div>
